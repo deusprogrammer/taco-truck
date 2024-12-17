83dbf7fc25d78cfcdd248db8cb212fe6ec8e4fc3
Michael Main <deusprogrammer@gmail.com>
2024-12-17 11:03:43 -0600

    Refactored menus a little bit

--- src/components/elements/Buttons.jsx
+++ src/components/elements/Buttons.jsx
@@ -1,6 +1,6 @@
 export const ModeButton = ({mode, currentMode, onClick}) => (
     <button 
-        className={`text-white w-64 h-20 ${currentMode === mode.toUpperCase() ? "bg-black" : "bg-slate-600"}`} 
+        className={`text-white w-64 h-20 ${currentMode === mode.toUpperCase() ? "bg-black" : "bg-slate-600"} border-solid border-2 border-black`} 
         onClick={(e) => {onClick(e, mode.toUpperCase())}}
     >
         {mode}
@@ -9,7 +9,7 @@ export const ModeButton = ({mode, currentMode, onClick}) => (
 
 export const SaveButton = ({onClick}) => (
     <button 
-        className={`text-white w-64 h-20 bg-slate-600`} 
+        className={`text-white w-64 h-20 bg-slate-600 border-solid border-2 border-black`} 
         onClick={onClick}
     >
         Save
@@ -18,7 +18,7 @@ export const SaveButton = ({onClick}) => (
 
 export const ImportButton = ({onClick}) => (
     <button 
-        className={`text-white w-64 h-20 bg-slate-600`} 
+        className={`text-white w-64 h-20 bg-slate-600 border-solid border-2 border-black`} 
         onClick={onClick}
     >
         Import
@@ -27,7 +27,7 @@ export const ImportButton = ({onClick}) => (
 
 export const PartSelectionButton = ({name, partId, partType, placingPart, onClick}) => (
     <button 
-        className={`text-white w-64 h-20 ${placingPart === partId ? "bg-black" : "bg-slate-600"}`} 
+        className={`text-white w-64 h-20 ${placingPart === partId ? "bg-black" : "bg-slate-600"} border-solid border-2 border-black`} 
         onClick={() => onClick(partType, partId)}
     >
         {name}
@@ -37,7 +37,7 @@ export const PartSelectionButton = ({name, partId, partType, placingPart, onClic
 export const ZoomButton = ({currentZoom, onZoomChange}) => (
     <>
         <button 
-            className={`text-white bg-slate-600 w-64 h-20`} 
+            className={`text-white bg-slate-600 w-64 h-20 border-solid border-2 border-black`} 
             disabled={currentZoom === 0.5}
             onClick={() => onZoomChange(-0.5)}
         >
@@ -47,7 +47,7 @@ export const ZoomButton = ({currentZoom, onZoomChange}) => (
             {currentZoom * 100}%
         </div>
         <button 
-            className={`text-white bg-slate-600 w-64 h-20`} 
+            className={`text-white bg-slate-600 w-64 h-20 border-solid border-2 border-black`} 
             onClick={() => onZoomChange(0.5)}
         >
             +
--- src/components/menus/PartDetailsMenu.jsx
+++ src/components/menus/PartDetailsMenu.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { partTable } from '../../data/parts.table';
 import { calculateRelativePosition } from '../utils';
 
 const PartDetailsMenu = ({layout, selectedPart, onUpdatePart, onSecondarySelectPart, onSetSecondarySelect}) => {
@@ -19,133 +18,128 @@ const PartDetailsMenu = ({layout, selectedPart, onUpdatePart, onSecondarySelectP
     }
 
     return (
-        <div className='flex flex-row justify-around items-center gap-10 px-2 absolute left-[0px] bottom-[0px] w-full z-50 overflow-y-scroll border-solid border-white bg-slate-400'>
-            <div className='flex flex-row gap-1 items-center'>
-                <label>Type:</label>
-                <input className='text-center' value={selectedPart?.type} />
-            </div>
-            <div className='flex flex-row gap-1 items-center'>
-                <label>Name:</label>
-                <input 
-                    className='text-center' 
-                    value={selectedPart?.name}
-                    onChange={({target: {value}}) => {
-                        onUpdatePart(selectedPart.id, {...selectedPart, name: value});
-                    }} 
-                />
-            </div>
-            <div className='flex flex-row gap-1 items-center'>
-                <label>Part Type:</label>
-                <select 
-                    className='text-center'
-                    value={selectedPart?.type}
-                    onChange={({target: {value}}) => {
-                        onUpdatePart(selectedPart.id, {...selectedPart, type: value})
-                    }}
-                >
-                    {Object.keys(selectedPart?.type === "custom" ? {custom : {custom: {}}} : partTable).map((key) => (
-                        <option key={key}>{key}</option>
-                    ))}
-                </select>
-            </div>
-            <div className='flex flex-row gap-1 items-center'>
-                <label>Part Id:</label>
-                <select 
-                    className='text-center'
-                    value={selectedPart?.partId}
-                    onChange={({target: {value}}) => {
-                        onUpdatePart(selectedPart.id, {...selectedPart, partId: value})
-                    }}
-                >
-                    {Object.keys(partTable[selectedPart?.type] || {custom: {}}).map((key) => (
-                        <option key={key}>{key}</option>
-                    ))}
-                </select>
-            </div>
-            <div className='flex flex-row gap-1 items-center'>
-                <label>rotation:</label>
-                <input 
-                    className='text-center' 
-                    value={selectedPart?.rotation || 0} 
-                    onChange={({target: {value}}) => {
-                        onUpdatePart(selectedPart.id, {...selectedPart, rotation: parseFloat(value)});
-                    }}
-                />
-            </div>
-            <div className='flex flex-row gap-1 items-center'>
-                <label>x:</label>
-                <input 
-                    className='text-center' 
-                    value={selectedPart?.position[0]} 
-                    onChange={({target: {value}}) => {
-                        onUpdatePart(selectedPart.id, {...selectedPart, position: [parseFloat(value), selectedPart.position[1]]});
-                    }}
-                />
-            </div>
-            <div className='flex flex-row gap-1 items-center'>
-                <label>y:</label>
-                <input 
-                    className='text-center' 
-                    value={selectedPart?.position[1]}
-                    onChange={({target: {value}}) => {
-                        onUpdatePart(selectedPart.id, {...selectedPart, position: [selectedPart.position[0], parseFloat(value)]});
-                    }}
-                />
-            </div>
-            <div className='flex flex-row gap-1 items-center'>
-                <label>Origin X:</label>
-                <input 
-                    className='text-center' 
-                    value={selectedPart?.origin[0]} 
-                    onChange={({target: {value}}) => {
-                        onUpdatePart(selectedPart.id, {...selectedPart, origin: [parseFloat(value), selectedPart.origin[1]]});
-                    }}
-                />
-            </div>
-            <div className='flex flex-row gap-1 items-center'>
-                <label>Origin Y:</label>
-                <input 
-                    className='text-center' 
-                    value={selectedPart?.origin[1]}
-                    onChange={({target: {value}}) => {
-                        onUpdatePart(selectedPart.id, {...selectedPart, origin: [selectedPart.origin[0], parseFloat(value)]});
-                    }}
-                />
-            </div>
-            <div className='flex flex-row gap-1 items-center'>
-                <label>Anchor X:</label>
-                <input 
-                    className='text-center' 
-                    value={selectedPart?.anchor?.[0] || 0} 
-                    onChange={({target: {value}}) => {
-                        onUpdatePart(selectedPart.id, {...selectedPart, anchor: [parseFloat(value), selectedPart?.anchor?.[1] || 0]});
-                    }}
-                />
-            </div>
-            <div className='flex flex-row gap-1 items-center'>
-                <label>Anchor Y:</label>
-                <input 
-                    className='text-center' 
-                    value={selectedPart?.anchor?.[1] || 0}
-                    onChange={({target: {value}}) => {
-                        onUpdatePart(selectedPart.id, {...selectedPart, anchor: [selectedPart?.anchor?.[0] || 0, parseFloat(value)]});
-                    }}
-                />
-            </div>
-            <div className='flex flex-row gap-1 items-center'>
-                <label>Relative to:</label>
-                <button 
-                    className={`h-8 p-1 min-w-20 ${onSecondarySelectPart ? 'bg-black text-white' : 'bg-white'}`}
-                    onClick={() => {
-                        onSetSecondarySelect(
-                            () => ((relativePart) => (
-                                onUpdatePart(selectedPart.id, {...adjustPositionToRelative(selectedPart, relativePart), relativeTo: relativePart.id})
-                            ))
-                        );
-                    }}
-                >
-                    {selectedPart?.relativeTo ? `${selectedPart?.name}` : "Select"}
-                </button>
+        <div className='flex flex-col gap-1 absolute right-[10px] top-[50%] max-h-[50%] max-w-[300px] p-2 overflow-y-auto translate-y-[-50%] bg-slate-400'>
+            <h2 className="text-center text-[1rem] font-bold">Part Details</h2>
+            <div className='flex flex-col gap-1 overflow-y-auto'>
+                <div className='flex flex-col gap-1'>
+                    <label>type:</label>
+                    <input value={selectedPart?.type} />
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <label>name:</label>
+                    <input  
+                        value={selectedPart?.name}
+                        onChange={({target: {value}}) => {
+                            onUpdatePart(selectedPart.id, {...selectedPart, name: value});
+                        }} 
+                    />
+                </div>
+                {/* <div className='flex flex-row gap-1 items-center'>
+                    <label>Part Type:</label>
+                    <select 
+                        className='text-center'
+                        value={selectedPart?.type}
+                        onChange={({target: {value}}) => {
+                            onUpdatePart(selectedPart.id, {...selectedPart, type: value})
+                        }}
+                    >
+                        {Object.keys(selectedPart?.type === "custom" ? {custom : {custom: {}}} : partTable).map((key) => (
+                            <option key={key}>{key}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className='flex flex-row gap-1 items-center'>
+                    <label>Part Id:</label>
+                    <select 
+                        className='text-center'
+                        value={selectedPart?.partId}
+                        onChange={({target: {value}}) => {
+                            onUpdatePart(selectedPart.id, {...selectedPart, partId: value})
+                        }}
+                    >
+                        {Object.keys(partTable[selectedPart?.type] || {custom: {}}).map((key) => (
+                            <option key={key}>{key}</option>
+                        ))}
+                    </select>
+                </div> */}
+                <div className='flex flex-col gap-1'>
+                    <label>relative to:</label>
+                    <button 
+                        className={`h-8 p-1 min-w-20 ${onSecondarySelectPart ? 'bg-black text-white' : 'bg-white'} border-solid border-2 border-black`}
+                        onClick={() => {
+                            onSetSecondarySelect(
+                                () => ((relativePart) => (
+                                    onUpdatePart(selectedPart.id, {...adjustPositionToRelative(selectedPart, relativePart), relativeTo: relativePart.id})
+                                ))
+                            );
+                        }}
+                    >
+                        {selectedPart?.relativeTo ? `${selectedPart?.name}` : "Select"}
+                    </button>
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <label>rotation:</label>
+                    <input 
+                        value={selectedPart?.rotation || 0} 
+                        onChange={({target: {value}}) => {
+                            onUpdatePart(selectedPart.id, {...selectedPart, rotation: parseFloat(value)});
+                        }}
+                    />
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <label>x:</label>
+                    <input 
+                        value={selectedPart?.position[0]} 
+                        onChange={({target: {value}}) => {
+                            onUpdatePart(selectedPart.id, {...selectedPart, position: [parseFloat(value), selectedPart.position[1]]});
+                        }}
+                    />
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <label>y:</label>
+                    <input 
+                        value={selectedPart?.position[1]}
+                        onChange={({target: {value}}) => {
+                            onUpdatePart(selectedPart.id, {...selectedPart, position: [selectedPart.position[0], parseFloat(value)]});
+                        }}
+                    />
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <label>origin x:</label>
+                    <input 
+                        value={selectedPart?.origin[0]} 
+                        onChange={({target: {value}}) => {
+                            onUpdatePart(selectedPart.id, {...selectedPart, origin: [parseFloat(value), selectedPart.origin[1]]});
+                        }}
+                    />
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <label>origin y:</label>
+                    <input 
+                        value={selectedPart?.origin[1]}
+                        onChange={({target: {value}}) => {
+                            onUpdatePart(selectedPart.id, {...selectedPart, origin: [selectedPart.origin[0], parseFloat(value)]});
+                        }}
+                    />
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <label>anchor x:</label>
+                    <input 
+                        value={selectedPart?.anchor?.[0] || 0} 
+                        onChange={({target: {value}}) => {
+                            onUpdatePart(selectedPart.id, {...selectedPart, anchor: [parseFloat(value), selectedPart?.anchor?.[1] || 0]});
+                        }}
+                    />
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <label>anchor y:</label>
+                    <input 
+                        value={selectedPart?.anchor?.[1] || 0}
+                        onChange={({target: {value}}) => {
+                            onUpdatePart(selectedPart.id, {...selectedPart, anchor: [selectedPart?.anchor?.[0] || 0, parseFloat(value)]});
+                        }}
+                    />
+                </div>
             </div>
         </div>
     );
