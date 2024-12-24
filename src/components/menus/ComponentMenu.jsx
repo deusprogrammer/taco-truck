f10dd38627708bc981eeb011ef9d1c830613eb6e
Michael Main <deusprogrammer@gmail.com>
2024-12-23 20:02:04 -0600

    Updated with labels and delete function

--- src/components/menus/ComponentMenu.jsx
+++ src/components/menus/ComponentMenu.jsx
@@ -8,9 +8,16 @@ const ComponentMenu = ({
     onLayoutChange,
     onSelect,
     onHover,
+    onDelete,
 }) => {
     const updatePanelSize = (dimensions) => {
         onLayoutChange({ ...layout, panelDimensions: dimensions })
+    }
+
+    const deleteComponent = (id) => {
+        const updatedParts = layout.parts.filter((part) => part.id !== id)
+        onLayoutChange({ ...layout, parts: updatedParts })
+        onSelect(null)
     }
 
     return (
@@ -19,7 +26,7 @@ const ComponentMenu = ({
                 Component Details
             </h2>
             <div className="flex flex-col gap-1 overflow-y-auto">
-                <h3>Name:</h3>
+                <label>Name:</label>
                 <input
                     value={layout?.name || ''}
                     placeholder="part name"
@@ -27,41 +34,56 @@ const ComponentMenu = ({
                         onLayoutChange({ ...layout, name: value })
                     }}
                 />
-                <h3>Panel:</h3>
+                <label>Width:</label>
                 <BufferedInput
                     value={layout?.panelDimensions[0] || 0}
                     onChange={(value) =>
                         updatePanelSize([value, layout?.panelDimensions[1]])
                     }
                 />
+                <label>Height:</label>
                 <BufferedInput
                     value={layout?.panelDimensions[1] || 0}
                     onChange={(value) =>
                         updatePanelSize([layout?.panelDimensions[0], value])
                     }
                 />
-                <h3>Parts:</h3>
+                <label>Parts:</label>
                 {[...Object.keys(partTable), 'custom'].map((key) => (
                     <React.Fragment key={key}>
                         <h4 className="bg-slate-300">{key.toUpperCase()}</h4>
                         {layout?.parts
                             ?.filter(({ type }) => key === type)
                             .map(({ id, type, partId, name }, index) => (
-                                <button
-                                    key={`element-${index}`}
-                                    className={`p-3 ${selectedPartId === id ? 'bg-black text-white' : 'bg-white'} hover:bg-slate-600 hover:text-white border-solid border-2 border-black`}
-                                    onClick={() => {
-                                        onSelect(id)
-                                    }}
-                                    onMouseEnter={() => {
-                                        onHover(id)
-                                    }}
-                                    onMouseOut={() => {
-                                        onHover(null)
-                                    }}
+                                <div
+                                    className="flex flex-row gap-0"
+                                    key={`part-${index}`}
                                 >
-                                    <b>{name}</b>({partId})
-                                </button>
+                                    <button
+                                        key={`element-${index}`}
+                                        className={`p-3 ${selectedPartId === id ? 'bg-black text-white' : 'bg-white'} hover:bg-slate-600 hover:text-white border-solid border-2 border-black`}
+                                        onClick={() => {
+                                            onSelect(id)
+                                        }}
+                                        onMouseEnter={() => {
+                                            onHover(id)
+                                        }}
+                                        onMouseOut={() => {
+                                            onHover(null)
+                                        }}
+                                    >
+                                        <b>{name}</b>({partId})
+                                    </button>
+                                    <button
+                                        key={`element-${index}`}
+                                        className={`p-3 ${selectedPartId === id ? 'bg-black text-white' : 'bg-white'} hover:bg-slate-600 hover:text-white border-solid border-2 border-black`}
+                                        onClick={() => {
+                                            deleteComponent(id)
+                                        }}
+                                    >
+                                        X
+                                    </button>
+                                </div>
                             ))}
                     </React.Fragment>
                 ))}
