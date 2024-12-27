3f4049a8d2c6c59378b12e73ffe070331f5e29ba
Michael Main <deusprogrammer@gmail.com>
2024-12-27 14:00:29 -0600

    Updated SVG to have artwork too

--- src/components/svg/PanelSvg.jsx
+++ src/components/svg/PanelSvg.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
 
 const PanelSvg = ({ layout, fill }) => (
-    <rect
-        x={0}
-        y={0}
-        rx={layout?.cornerRadius || 0}
-        ry={layout?.cornerRadius || 0}
-        width={layout?.panelDimensions?.[0]}
-        height={layout?.panelDimensions?.[1]}
-        stroke="black"
-        strokeWidth={1}
-        fill="white"
-    />
+    <>
+        <defs>
+            <clipPath id="panelClip">
+                <rect
+                    x={0}
+                    y={0}
+                    rx={layout?.cornerRadius || 0}
+                    ry={layout?.cornerRadius || 0}
+                    width={layout?.panelDimensions?.[0]}
+                    height={layout?.panelDimensions?.[1]}
+                />
+            </clipPath>
+        </defs>
+        <rect
+            x={0}
+            y={0}
+            rx={layout?.cornerRadius || 0}
+            ry={layout?.cornerRadius || 0}
+            width={layout?.panelDimensions?.[0]}
+            height={layout?.panelDimensions?.[1]}
+            stroke="black"
+            strokeWidth={1}
+            fill="black"
+        />
+        {layout.artwork && (
+            <image
+                href={layout.artwork}
+                width={layout?.panelDimensions?.[0]}
+                height={layout?.panelDimensions?.[1]}
+                clipPath="url(#panelClip)"
+            />
+        )}
+    </>
 )
 
 export default PanelSvg
