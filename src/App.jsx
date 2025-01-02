bf9327d779f3d1fa5a77275fcd4b7b165a0c08ac
Michael Main <deusprogrammer@gmail.com>
2025-01-02 02:50:20 -0600

    Trying to improve touch controls for panning

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,8 @@ import Home from './routes/Home'
 import PartDesignerRoute from './routes/PartDesignerRoute'
 import ComponentManagerRoute from './routes/ComponentManagerRoute'
 import './App.css'
-import { useEffect } from 'react'
 
 const App = () => {
-    useEffect(() => {
-        // Prevent overscroll
-        document.body.style.overflow = 'hidden'
-        document.documentElement.style.overflow = 'hidden'
-
-        // Prevent overscroll on iOS
-        const preventOverscroll = (event) => {
-            if (event.touches.length > 1) {
-                event.preventDefault()
-            }
-        }
-
-        document.addEventListener('touchmove', preventOverscroll, {
-            passive: false,
-        })
-
-        return () => {
-            // Clean up the styles and event listener when the component unmounts
-            document.body.style.overflow = ''
-            document.documentElement.style.overflow = ''
-            document.removeEventListener('touchmove', preventOverscroll)
-        }
-    }, [])
-
     return (
         <BrowserRouter>
             <Routes>
