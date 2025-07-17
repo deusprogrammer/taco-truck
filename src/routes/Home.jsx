446eed47ea72a677c4b01d8f744fb6fde9deff59
Michael Main <deusprogrammer@gmail.com>
2025-07-16 21:00:35 -0500

    Reverting home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router'
 import config from '../../package.json'
 import { login } from '../components/utils'
 import { useSecurity } from '../contexts/SecurityContext'
-import AboutModal from '../components/menus/About'
 
 const Home = () => {
     const securityContext = useSecurity()
-    const [aboutOpen, setAboutOpen] = useState(false)
 
     return (
-        <div className="flex h-screen flex-col items-center justify-center gap-1 bg-slate-600 text-white">
+        <div className="flex h-screen flex-col items-center justify-center gap-10 bg-slate-600 text-white">
             <img
                 className="w-[200px]"
                 alt="logo"
@@ -19,58 +17,22 @@ const Home = () => {
             <h1 className="text-[2rem]">
                 Project Taco Truck v{config.version}
             </h1>
-
-            <h2 className="text-[1.5rem]">Design</h2>
-            <Link
-                className="bg-slate-500 p-2 hover:bg-slate-800"
-                to={`/designer`}
-            >
-                Project/Layout Designer
+            <Link className="p-4 text-[1.5rem]" to={`/designer`}>
+                Layout/Panel Designer
             </Link>
-            <Link
-                className="bg-slate-500 p-2 hover:bg-slate-800"
-                to={`/designer/complex-parts`}
-            >
+            <Link className="p-4 text-[1.5rem]" to={`/designer/complex-parts`}>
                 Part Designer
             </Link>
-
-            <h2 className="text-[1.5rem]">Browse</h2>
-            <Link
-                className="bg-slate-500 p-2 hover:bg-slate-800"
-                to={`/manager`}
-            >
-                Project/Layout List
+            <Link className="p-4 text-[1.5rem]" to={`/manager`}>
+                Manager
             </Link>
-
-            <h2 className="text-[1.5rem]">User</h2>
             {securityContext ? (
                 <div>Welcome back {securityContext.username}</div>
             ) : (
-                <button
-                    className="bg-slate-500 p-2 hover:bg-slate-800"
-                    onClick={login}
-                >
+                <button className="p-4 text-[1.5rem]" onClick={login}>
                     Login
                 </button>
             )}
-
-            <h2 className="text-[1.5rem]">About</h2>
-            <button
-                className="bg-slate-500 p-2 hover:bg-slate-800"
-                onClick={() => setAboutOpen(true)}
-            >
-                About
-            </button>
-            <button
-                onClick={() =>
-                    window.open('https://ko-fi.com/michaelcmain52278')
-                }
-                className="bg-slate-500 p-2 hover:bg-slate-800"
-            >
-                Donate =3
-            </button>
-            <AboutModal open={aboutOpen} onClose={() => setAboutOpen(false)} />
-
             <footer>© 2025 Michael C Main</footer>
         </div>
     )
